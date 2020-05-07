4ade71d55f5edabaa57f6026acc52e4e03d5e45e
Zoe Anderson <zoeand398@gmail.com>
2020-05-06 21:58:27 -0400

    basic nodes

--- ast.py
+++ ast.py
@@ -23,7 +23,61 @@ class Int():
         self.value = value
     def __str__(self):
         return str(self.value)
-    def __type__(self):
+    def get_type(self):
         return INT
-    def get_value(self):
-        return self.value
+    # def evaluate(self):
+    #     return self.value
+
+class Bool():
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return "true" if self.value else "false"
+    def get_type(self):
+        return BOOL
+    # def evaluate(self):
+    #     return self.value
+
+class And():
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+    def __str__(self):
+        return "(" + str(self.left) + ")" + " && " + "(" + str(self.right) + ")"
+    def get_type(self):
+        return AND
+    # def evaluate(self, environment):
+    #     return self.left.evaluate(environment) and self.right.evaluate(environment)
+
+class Or():
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+    def __str__(self):
+        return "(" + str(self.left) + ")" + " || " + "(" + str(self.right) + ")"
+    def get_type(self):
+        return OR
+
+class Not():
+    def __init__(self, child):
+        self.child = child
+    def __str__(self):
+        return "!" + "(" + str(self.child) + ")"
+    def get_type(self):
+        return NOT
+
+class Flse():
+    def __init__(self):
+        self.value = False
+    def __str__(self):
+        return "false"
+    def get_type(self):
+        return FALSE
+
+class Tru():
+    def __init__(self):
+        self.value = True
+    def __str__(self):
+        return "true"
+    def get_type(self):
+        return TRUE
