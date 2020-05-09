5772b1c7a1b3bcf2acd1eb7aa2be30da40cc4bf7
Zoe Anderson <zoeand398@gmail.com>
2020-05-09 00:19:12 -0400

    changed type tostrings, deubging func

--- AST_changed.py
+++ AST_changed.py
@@ -31,42 +31,59 @@ class Type:
     def __init__(self):
         raise Exception('')
 
+# I think when we print these types, we want them like how scala expects them
+# so I'm going to change LIST -> List etc
+# and change the __str__ to a get_type in case we need that
+# also we don't actually need inits if we don't do anything in them
 class TList(Type):
     def __init__(self):
         pass
-    def __str__(self):
+    def get_type(self):
         return "LIST"
+    def __str__(self):
+        return "List"
 
 class TSet(Type):
     def __init__(self, type_):
         pass
-    def __str__(self):
+    def get_type(self):
         return "SET"
+    def __str__(self):
+        return "Set"
 
 class TInt(Type):
     def __init__(self):
         pass
-    def __str__(self):
+    def get_type(self):
         return "INT"
+    def __str__(self):
+        return "Int"
 
 class TBool(Type):
     def __init__(self):
         pass
-    def __str__(self):
+    def get_type(self):
         return "BOOL"
+    def __str__(self):
+        return "Boolean"
 
 class TArrow(Type):
     def __init__(self, t1, t2):
         self.from_ = t1
         self.to_ = t2
-    def __str__(self):
+    def get_type(self):
         return str(self.from_) + " -> " + str(self.to_)
+    def __str__(self):
+        return self.get_type() # for now
 
 class TTuple(Type):
+    # what is this for?
     def __init__(self, types):
         self.types = types
-    def __str__(self):
+    def get_type(self):
         return "TUPLE(" + list_str(self.types) + ')'
+    def __str__(self):
+        return "(" + list_str(self.types) + ")"
 
 
 INT = TInt()
@@ -75,12 +92,20 @@ BOOL = TBool()
 SET = TSet(INT)
 
 class Var():
+    # I know we have the problem of variables that are inputs in functions,
+    # but I think there's cases where it makes more sense for a Var to have a name and a value
+    # and then the value is its own node and has a type
+    # not the environment way
+    # like what we talked about this morning?
     def __init__(self, name):
         self.name = name
+        # self.value = value
     def __str__(self):
-        return self.name
+        return "var " + self.name #+ " = " + str(self.value)
     def get_type(self, envt):
         return envt[name].get_type()
+    # def get_type(self):
+    #     return self.value.get_type()
 
 class Int():
     def __init__(self, value):
@@ -192,13 +217,14 @@ class Tru():
     def get_type(self, envt):
         return BOOL
 
-# class Lst():
-#     def __init__(self, value):
-#         self.value = value # Cons or Nil
-#     def __str__(self):
-#         return str(self.value)
-#     def get_type(self, envt):
-#         return LIST
+# I think we do need a list type. Leon has a list type.
+class Lst():
+    def __init__(self, value):
+        self.value = value # Cons or Nil
+    def __str__(self):
+        return str(self.value)
+    def get_type(self, envt):
+        return LIST
 
 class Cons():
     def __init__(self, car, cdr):
@@ -227,6 +253,7 @@ class Nil():
 #         return CASE
 
 class Match():
+    # I guess if the only thing we ever want to match on is lists, this is fine
     def __init__(self, match_on, nil_case, cons_vars, cons_case):
         self.match_on = match_on
         self.nil_case = nil_case
@@ -242,7 +269,6 @@ class Match():
         return TArrow(match_on.get_type(), nil_case.get_type())
 
 class Hole():
-
     def __init__(self, type_):
         self.type = type_
     def __str__(self):
@@ -274,8 +300,21 @@ class Func():
         self.var_types = var_types
         self.ret_type = ret_type
         self.body = body
+    def _get_function_arguments(self):
+        if len(self.vars) == 0:
+            return ""
+
+        arguments = ""
+        for i in range(len(self.vars) -1):
+            arguments += str(self.vars[i]) + " : " + str(self.var_types[i]) + ", "
+        arguments += str(self.vars[-1]) + " : " + str(self.var_types[-1])
+        return arguments
+
     def __str__(self):
-        return 'func' + list_str(self.vars) + ": " + list_str(self.var_types) + ' = ' + str(self.body)
+        # this has a bug, multiple parameters won't be next to their type
+        # also, shouldn't this get pretty printed the way scala expects it, like with scala types like List instead of LIST etc?
+
+        return 'def ' + self.name + " (" + self._get_function_arguments() + ") " + ": " + str(self.ret_type) + ' = ' + str(self.body)
     def get_type(self, envt):
         return TArrow(var_types, ret_type)
 
@@ -338,34 +377,34 @@ class Harness():
 
 
 if __name__ == '__main__':
-    size = Func('size', [LIST], INT, ['lst'], 
+    size = Func('size', [LIST, INT], INT, ['lst', Int(0)],
         Match(Var('lst'),
             Int(0),
             ['_', 'rest'], Plus(Int(1), App(Var('size'), [Var('rest')]))
         )
     )
-    content = Func('content', [LIST], INT, ['lst'],
-        Match(Var('lst'),
-            St([]),
-            ['e', 'rest'], StPlus(St(['e']), App(Var('content'), [Var('rest')]))
-            )
-    )
-    split0 = Harness('split', [LIST], TTuple([LIST, LIST]), ['lst'],
-        Func('_', [TTuple([LIST, LIST])], BOOL, ['r'],
-            Eq(App(Var('content'), Var('lst')), StPlus(App(Var('content'), TupleAcc(Var('r'), 0)), App(Var('content'), TupleAcc(Var('r'), 1))))),
-        Match(Var('lst'),
-            Tuple([Nil(), Nil()]),
-            ['h', 't'], Match(Var('t'),
-                Tuple([Nil(), Cons(Var('h'), Nil())]),
-                ['h2', 't2'],
-                LetIn('v', App(Var('split'), [Var('t2')]),
-                    Tuple([Cons(Var('h1'), TupleAcc(Var('r'), 1)), Cons(Var('h2'), TupleAcc(Var('r'), 2))])
-                )
-            )
-        )
-    )
+    # content = Func('content', [LIST], INT, ['lst'],
+    #     Match(Var('lst'),
+    #         St([]),
+    #         ['e', 'rest'], StPlus(St(['e']), App(Var('content'), [Var('rest')]))
+    #         )
+    # )
+    # split0 = Harness('split', [LIST], TTuple([LIST, LIST]), ['lst'],
+    #     Func('_', [TTuple([LIST, LIST])], BOOL, ['r'],
+    #         Eq(App(Var('content'), Var('lst')), StPlus(App(Var('content'), TupleAcc(Var('r'), 0)), App(Var('content'), TupleAcc(Var('r'), 1))))),
+    #     Match(Var('lst'),
+    #         Tuple([Nil(), Nil()]),
+    #         ['h', 't'], Match(Var('t'),
+    #             Tuple([Nil(), Cons(Var('h'), Nil())]),
+    #             ['h2', 't2'],
+    #             LetIn('v', App(Var('split'), [Var('t2')]),
+    #                 Tuple([Cons(Var('h1'), TupleAcc(Var('r'), 1)), Cons(Var('h2'), TupleAcc(Var('r'), 2))])
+    #             )
+    #         )
+    #     )
+    # )
 
-    print(split0)
+    print(size)
     pass
     # print(Choose(Annotation("r", Lst(Cons(2, Nil()))), Not(Or(Tru(), Flse()))))
     # print(Match(Lst(Nil()), [Case(Cons(PatternValue("h"), Nil()), Flse())]))
